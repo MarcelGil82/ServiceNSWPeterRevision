6117308839dc9a85485490efe06afe44ed1dc4ef
Marcel Gil <Marcel.Gil1@studytafensw.edu.au>
2020-06-01 16:43:13 +1000

    revision with Peter

---
+++ PeterRevision.js
@@ -0,0 +1,22 @@
+// let myVariable
+// myVariable = "65415"; 
+// let convertNumber = Number(myVariable);
+// // console.log(convertNumber);
+// // console.log(typeof convertNumber);
+
+// let myOtherVariable;
+// myOtherVariable = true;
+// let convertToOtherNumber = Number(myOtherVariable)
+// // console.log(convertToOtherNumber);
+// // console.log(typeof convertToOtherNumber)
+
+
+// let myVariableAndMyOtherVariable = convertNumber + convertToOtherNumber;
+// console.log(myVariableAndMyOtherVariable);
+// console.log(typeof myVariableAndMyOtherVariable)
+
+let x = true;
+let y = "Hi";
+
+let areTheSame = (typeof x == typeof y);
+console.log(areTheSame)
